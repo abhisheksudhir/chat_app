9c27faa78b10788f7db0c9bfe7f430717d8a923c
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-06-18 20:03:27 +0530

    implementing authentication

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -7,6 +7,7 @@ class AuthForm extends StatefulWidget {
     String password,
     String userName,
     bool isLogin,
+    BuildContext ctx,
   ) submitFn;
 
   AuthForm(this.submitFn);
@@ -29,10 +30,11 @@ class _AuthFormState extends State<AuthForm> {
     if (isValid) {
       _formKey.currentState.save();
       widget.submitFn(
-        _userEmail,
+        _userEmail.trim(),
         _userPassword,
-        _userName,
+        _userName.trim(),
         _isLogin,
+        context,
       );
     }
     // use values to send auth request
